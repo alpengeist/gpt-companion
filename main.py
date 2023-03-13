cabf1378ef18be270d446b634b3e816f864f8883
alpen <alpen.hermann@gmail.com>
2023-03-13 10:45:33 +0100

    next try: pyautogui for cmd-c keypress

--- main.py
+++ main.py
@@ -109,7 +109,7 @@ def paste_and_complete():
 #        with c.pressed(pynput.keyboard.Key.cmd.value):
 #            c.press('c')
 #            c.release('c')
-        pyautogui.hotkey('cmd', 'c')
+        pyautogui.hotkey('command', 'c')
     else:
 #        with c.pressed(pynput.keyboard.Key.ctrl):
 #            c.press('c')
